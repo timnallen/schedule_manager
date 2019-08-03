b659d29bc29a89e90040b8256f92f41d84358431
Tim Allen <tnelsallen3@gmail.com>
2019-08-03 13:00:28 -0600

    Add Request spec for a single schedule AND factory bot

---
+++ spec/requests/api/v1/user_can_see_a_schedule_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Schedule API' do
+  describe 'When I go to a schedule show endpoint' do
+    it 'shows me the schedule' do
+      id = create(:schedule).id
+
+      get "/api/v1/schedules/#{id}"
+
+      expect(response).to be_successful
+      schedule = JSON.parse(response.body)
+      expect(schedule['data'].id).to eq(id)
+      expect(schedule['data'].type).to eq("schedule")
+      expect(schedule['data'].attributes).to eq({"appointments": []})
+    end
+  end
+end
