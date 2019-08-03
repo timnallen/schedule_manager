23657da396d1215bf4e842903c114bf16310be36
Tim Allen <tnelsallen3@gmail.com>
2019-08-03 16:15:10 -0600

    Add validation specs for Appt

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe Appointment do
+  describe 'validations' do
+    it { should validate_presence_of :start_time}
+    it { should validate_presence_of :end_time}
+  end
+
   describe 'relationships' do
     it { should belong_to :schedule}
   end
