9f13c16a0c2c394e76f46dac201c3ed7efba822c
Tim Allen <tnelsallen3@gmail.com>
2019-08-03 14:15:22 -0600

    Replace extra AR call with variable

--- app/controllers/api/v1/schedules_controller.rb
+++ app/controllers/api/v1/schedules_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SchedulesController < ApplicationController
   def show
     schedule = Schedule.find_by(id: params[:id])
     if schedule
-      render json: ScheduleSerializer.new(Schedule.find(params[:id]))
+      render json: ScheduleSerializer.new(schedule)
     else
       four_oh_four
     end
