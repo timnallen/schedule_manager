d6d58cd467a1f2129ba0eed8c9d74f9735bd7e51
Tim Allen <tnelsallen3@gmail.com>
2019-08-03 16:16:21 -0600

    Add create action for appt with helper methods

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,6 +1,26 @@
 class Api::V1::AppointmentsController < ApplicationController
   def show
-    appointment = Appointment.find_by(id: params[:id])
-    appointment ? render(json: AppointmentSerializer.new(appointment)) : four_oh_four
+    appt = Appointment.find_by(id: params[:id])
+    appt ? render_appt(appt) : four_oh_four
+  end
+
+  def create
+    sch = Schedule.find_by(id: params[:schedule_id])
+    sch ? schedule_appt(sch) : four_oh_four
+  end
+
+  private
+
+  def render_appt(appt, code = 200)
+    render json: AppointmentSerializer.new(appt), status: code
+  end
+
+  def schedule_appt(sch)
+    appt = sch.appointments.new(appt_params)
+    appt.save ? render_appt(appt, 201) : four_oh_four
+  end
+
+  def appt_params
+    params.permit(:start_time, :end_time)
   end
 end
