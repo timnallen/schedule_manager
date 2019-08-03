0fb97616794b1bd03d09557f46178e23b720b489
Tim Allen <tnelsallen3@gmail.com>
2019-08-03 15:38:12 -0600

    Add show action and serializer for appointment

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::AppointmentsController < ApplicationController
   def show
-    
+    appointment = Appointment.find_by(id: params[:id])
+    appointment ? render(json: AppointmentSerializer.new(appointment)) : four_oh_four
   end
 end
